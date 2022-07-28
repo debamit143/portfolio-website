d65afe5480417cce4fdb5ef3f40b6f3eb5fa3548
Amit Kumar Debnath <debamit143@gmail.com>
2022-07-29 05:08:25 +0530

    Work  Section Added

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Experience.css'
+
+export const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="achievement">
+                <div className="circle">1.5+</div>
+                <span>Years</span>
+                <span>Experience</span>
+            </div>
+            <div className="achievement">
+                <div className="circle">3+</div>
+                <span>Completed</span>
+                <span>Projects</span>
+            </div>
+            <div className="achievement">
+                <div className="circle">1</div>
+                <span>Companies</span>
+                <span>Work</span>
+            </div>
+        </div>
+    )
+}
